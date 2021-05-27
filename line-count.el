e8df167c6ed72d358dbf11ff731aae9ede8cf486
Veera Sivarajan <sveera.2001@gmail.com>
2021-05-27 01:33:41 -0400

    Make line-count() more modular
    Now, fetching text is seperated from processing the results. This
    has reduced the number of lines repeated. Some bugs needs fixing.

--- line-count.el
+++ line-count.el
@@ -1,4 +1,4 @@
-(defun get-selected-text (start end)
+(defun text-from-region (start end)
   (let ((regionp (buffer-substring-no-properties start end)))
     (split-string regionp "\n"))) 
 
@@ -11,26 +11,20 @@
 (defun isNotEmptyStr (input_str)
   (not (string= input_str ""))) 
 
-(defun line-count (start end)
-  (interactive "r")
-  (if (use-region-p)
-      (let* ((text (get-selected-text start end))
-             (total-len (length text))
-             (filter-len (length (my_filter text 'isNotEmptyStr)))
-             (empty-len (- total-len filter-len)))
-        (message "Lines: %d, Empty: %d" filter-len (- empty-len 1)))
-    (message "No lines selected"))) 
-
 (defun read-lines (filePath)
   "Return a list of lines of a file at filePath."
   (with-temp-buffer
     (insert-file-contents filePath)
     (split-string (buffer-string) "\n"))) 
 
-(defun other-line-count ()
-  (interactive)
-  (let* ((text (read-lines (buffer-file-name (current-buffer))))
-         (total-len (length text))
+(defun print-result (text)
+  (let* ((total-len (length text))
          (filter-len (length (my_filter text 'isNotEmptyStr)))
          (empty-len (- total-len filter-len)))
     (message "Lines: %d, Empty: %d" filter-len (- empty-len 1)))) 
+
+(defun line-count (start end)
+  (interactive "r")
+  (if (use-region-p)
+      (print-result (get-selected-text start end))
+    (print-result (read-lines (buffer-file-name (current-buffer)))))) 
